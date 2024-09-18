b41a15ec66d8f25a0042eb509a8cc9ef96d14895
patriciagloria <patglo2005@gmail.com>
2024-09-18 14:18:47 +0700

    menyelesaikan tutorial 3 yaitu login logout

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3fe)r%hy1+q6k62kpeq*tj_8vf$@weo3a70#uwm#fb=#0%kx(w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","patricia-gloria-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
